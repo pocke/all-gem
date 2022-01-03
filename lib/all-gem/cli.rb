ab6eb3c3a3c48a403b60867aa871b1eab4c8cb27
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2022-01-03 16:24:23 +0900

    WIP Implementing the CLI

--- lib/all-gem/cli.rb
+++ lib/all-gem/cli.rb
@@ -1,5 +1,14 @@
 module AllGem
   class CLI
+    class Error < StandardError
+      attr_reader :status
+
+      def initialize(message, status = 1)
+        super(message)
+        @status = status
+      end
+    end
+
     attr_reader :stdout, :stderr, :stdin
 
     def initialize(stdout:, stderr:, stdin:)
@@ -8,8 +17,19 @@ module AllGem
       @stdin = stdin
     end
 
-    def run
-      1
+    def run(argv)
+      # TODO: optparse
+      command = argv[0] or raise Error.new(help)
+
+      # TODO: find gem name from the command
+      # TODO: find all versions of the gem
+      # TODO: execute the command with each version
+    end
+
+    def help
+      <<~HELP
+        Usage: all-ruby [all-ruby options] COMMAND_NAME [command options]
+      HELP
     end
   end
 end
